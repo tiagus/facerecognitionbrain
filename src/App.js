ff098025aa8f614cadc28e37765bb536ed4e3a21
Tiago Santos <tiago@convertodigital.com>
2019-10-28 11:35:04 +0000

    Added detection for the first face found on the image

--- src/App.js
+++ src/App.js
@@ -30,8 +30,27 @@ class App extends Component {
     this.state = {
       input: "",
       imageUrl: "",
+      box: {},
     }
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  } 
 
   onImputChange = (event)=> {
     this.setState({input: event.target.value});
@@ -43,14 +62,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   }
 
   render(){
@@ -66,7 +79,7 @@ class App extends Component {
           onImputChange={this.onImputChange} 
           onButtonSubmit={this.onButtonSubmit} 
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
